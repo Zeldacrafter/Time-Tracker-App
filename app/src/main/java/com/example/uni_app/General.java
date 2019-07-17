9819fe9636679c874c930e8a695d19cc8e8be011
Alexander Pfanne <ayko12345@gmail.com>
2019-07-18 00:47:25 +0200

    Changing to Java 8, Adding a ListView to the Home Layout

---
+++ app/src/main/java/com/example/uni_app/General.java
@@ -0,0 +1,15 @@
+package com.example.uni_app;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class General {
+
+    public static void showToast(String msg, Context context){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showToast(String msg, Context context, int duration){
+        Toast.makeText(context, msg, duration).show();
+    }
+}
--- app/src/main/java/com/example/uni_app/activities/MainActivity.java
+++ app/src/main/java/com/example/uni_app/activities/MainActivity.java
@@ -12,17 +12,24 @@ import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.uni_app.General.showToast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     private Menu menu;
-    private MenuItem devDbViewer,
-            devVokCheat,
-            devReloadDatabaseAssets,
-            devReloadDatabaseIterative,
-            devShowSharedPrefrences;
+    private MenuItem devDbViewer;
+    private MenuItem addCategory;
+
+    private ListView categoryList;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    categoryList.setVisibility(View.VISIBLE);
                     return true;
                 case R.id.navigation_calendar:
+                    categoryList.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_settings:
+                    categoryList.setVisibility(View.GONE);
                     return true;
             }
             return false;
@@ -47,16 +57,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        categoryList = findViewById(R.id.home_category_list);
+        categoryList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>()));
+
+        TextView tv = new TextView(getApplicationContext());
+        tv.setText("ABC");
+        tv.setOnClickListener(view -> showToast("Hi", getApplicationContext()));
+        //categoryList.addHeaderView(tv);
+        categoryList.addFooterView(tv, null, true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Menu bar at top
         getMenuInflater().inflate(R.menu.top_menu, menu);
-
         this.menu = menu;
         devDbViewer = this.menu.findItem(R.id.menu_dev_db_viewer);
-
+        addCategory = this.menu.findItem(R.id.menu_add_category);
         return true;
     }
 
@@ -64,9 +82,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case (R.id.menu_dev_db_viewer):
-                Intent dbmanager = new Intent(this, DevAndroidDatabaseManager.class);
-                startActivity(dbmanager);
+                Intent dbManager = new Intent(this, DevAndroidDatabaseManager.class);
+                startActivity(dbManager);
                 break;
+            case (R.id.menu_add_category):
+                showToast("Item Added!", getApplicationContext());
             default:
                 Log.e(TAG, "Did not find menu item");
         }
