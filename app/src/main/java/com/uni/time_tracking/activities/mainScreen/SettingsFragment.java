fd725ce96db94727e794fffb04ba70788432f4ad
Alexander Pfanne <ayko12345@gmail.com>
2019-08-02 23:32:32 +0200

    Added Preferences

--- app/src/main/java/com/uni/time_tracking/activities/mainScreen/SettingsFragment.java
+++ app/src/main/java/com/uni/time_tracking/activities/mainScreen/SettingsFragment.java
@@ -9,6 +9,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
