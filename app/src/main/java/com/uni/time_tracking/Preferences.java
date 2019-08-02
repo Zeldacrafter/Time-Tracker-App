d9df643e5105d10221771a2b3106748a927da648
Alexander Pfanne <ayko12345@gmail.com>
2019-08-02 23:47:50 +0200

    Getters & Default values of preferences

---
+++ app/src/main/java/com/uni/time_tracking/Preferences.java
@@ -0,0 +1,28 @@
+package com.uni.time_tracking;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+public class Preferences {
+
+    public static final String KEY_SIMULTANEOUS_ACTIVITIES = "simultaneous_activities";
+    public static final String KEY_REMOVE_SHORT_ENTRIES = "remove_short_entries";
+    public static final String KEY_DARK_MODE = "dark_mode";
+
+    public static boolean isSimultaneousActivitiesAllowed(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(KEY_SIMULTANEOUS_ACTIVITIES, true);
+    }
+
+    public static boolean removeShortEntries(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(KEY_REMOVE_SHORT_ENTRIES, false);
+    }
+
+    public static boolean isDarkMode(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(KEY_DARK_MODE, false);
+    }
+}
