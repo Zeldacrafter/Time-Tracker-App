db8011d3fda4072be4c24ee248a761d207f1ed84
Alexander Pfanne <ayko12345@gmail.com>
2019-09-10 16:58:23 +0200

    Displaying (random) data as a bar-chart

--- app/src/main/java/com/uni/time_tracking/activities/mainScreen/EvaluationFragment.java
+++ app/src/main/java/com/uni/time_tracking/activities/mainScreen/EvaluationFragment.java
@@ -5,18 +5,104 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.uni.time_tracking.R;
 
-public class EvaluationFragment extends Fragment {
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class EvaluationFragment extends Fragment implements OnChartValueSelectedListener {
 
     private static final String TAG = "EvaluationFragment";
+
+    @BindView(R.id.evaluation_bar_chart) protected BarChart chart;
+    private Unbinder unbinder;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        chart.getDescription().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getLegend().setWordWrapEnabled(true);
+
+        setData();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_evaluation, container, false);
+        View v = inflater.inflate(R.layout.fragment_main_evaluation, container, false);
+        unbinder = ButterKnife.bind(this, v);
+        return v;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private void setData() {
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++){
+            ArrayList<BarEntry> values = new ArrayList<>();
+            values.add(new BarEntry(i, (float) (Math.random() * (30 + 1))));
+
+            int r = rand.nextInt(255);
+            int g = rand.nextInt(255);
+            int b = rand.nextInt(255);
+            int color = (0xff) << 24 | (r & 0xff) << 16 | (g & 0xff) << 8 | (b & 0xff);
+
+            BarDataSet set1 = new BarDataSet(values, randomString());
+            set1.setDrawIcons(false);
+            set1.setColor(color);
+
+            dataSets.add(set1);
+        }
+
+        BarData data = new BarData(dataSets);
+        data.setValueTextSize(10f);
+        data.setBarWidth(0.9f);
+
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+    public String randomString() {
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return generatedString;
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
 }
