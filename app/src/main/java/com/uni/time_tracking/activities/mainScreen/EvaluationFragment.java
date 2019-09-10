a7a9aea9bfb93bc9312a93d3b563d67c053bee45
Alexander Pfanne <ayko12345@gmail.com>
2019-09-10 23:22:00 +0200

    Bar Chart to display data

--- app/src/main/java/com/uni/time_tracking/activities/mainScreen/EvaluationFragment.java
+++ app/src/main/java/com/uni/time_tracking/activities/mainScreen/EvaluationFragment.java
@@ -1,7 +1,9 @@
 package com.uni.time_tracking.activities.mainScreen;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +13,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.uni.time_tracking.R;
 import com.uni.time_tracking.Time;
 import com.uni.time_tracking.database.DBHelper;
@@ -40,20 +48,31 @@ public class EvaluationFragment extends Fragment implements OnChartValueSelected
 
     private static final String TAG = "EvaluationFragment";
 
-    @BindView(R.id.evaluation_bar_chart) protected BarChart chart;
+    @BindView(R.id.evaluation_bar_chart) protected BarChart barChart;
+    @BindView(R.id.evaluation_pie_chart) protected PieChart pieChart;
     private Unbinder unbinder;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        chart.getDescription().setEnabled(false);
-        chart.getAxisRight().setEnabled(false);
-        chart.getXAxis().setEnabled(false);
-        chart.getLegend().setWordWrapEnabled(true);
+        barChart.getDescription().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getXAxis().setEnabled(false);
+        barChart.getLegend().setWordWrapEnabled(true);
+        setDataBarChart();
 
-        setData();
+        pieChart.getLegend().setEnabled(true);
+        pieChart.getLegend().setWordWrapEnabled(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setUsePercentValues(true);
+        pieChart.setDrawCenterText(false);
+        pieChart.setDrawEntryLabels(false);
+        setDataPieChart();
+        pieChart.getData().setValueTextSize(15);
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,7 +88,7 @@ public class EvaluationFragment extends Fragment implements OnChartValueSelected
         unbinder.unbind();
     }
 
-    private void setData() {
+    private void setDataBarChart() {
 
         DBHelper dbHelper = DBHelper.getInstance(getContext());
 
@@ -106,8 +125,40 @@ public class EvaluationFragment extends Fragment implements OnChartValueSelected
         data.setValueTextSize(10f);
         data.setBarWidth(0.9f);
 
-        chart.setData(data);
-        chart.invalidate();
+        barChart.setData(data);
+        barChart.invalidate();
+
+        dbHelper.close();
+    }
+
+    private void setDataPieChart() {
+        DBHelper dbHelper = DBHelper.getInstance(getContext());
+
+        ArrayList<PieEntry> values = new ArrayList<>();
+        ArrayList<Integer> colors = new ArrayList<>();
+        ActivityDB[] activities = dbHelper.getActiveActivities();
+
+        for (ActivityDB activity : activities){
+
+            float totalTimeSpent = 0;
+            TimeDB[] entries = dbHelper.getEntriesInTimespan(
+                    activity.getId(),
+                    Time.getCurrentTime().minusDays(7),
+                    Time.getCurrentTime());
+
+            for(TimeDB entry : entries) {
+                totalTimeSpent += getDifferenceLong(new Period(entry.getStart(), entry.getEnd())) / 10000.0f;
+            }
+
+            values.add(new PieEntry(totalTimeSpent, activity.getName()));
+            colors.add(activity.getColor());
+        }
+        PieDataSet set = new PieDataSet(values, "");
+        set.setDrawIcons(false);
+        set.setColors(colors);
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.invalidate();
 
         dbHelper.close();
     }
